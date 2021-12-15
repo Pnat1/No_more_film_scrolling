316efbd6d8cb52fb282e94715ca92cb12b691a4d
Pnat1 <gperinatcarvajal@gmail.com>
2021-12-15 02:56:13 +0100

    progress and organization

--- src/functions.py
+++ src/functions.py
@@ -11,3 +11,14 @@ def cleanstryear(x):
         return x
 
 
+def movie_recommend(original_title):
+    '''
+    Fuction computes top 10 recommendation given a movie title
+    '''
+    idx = indices[original_title]
+    sim_scores = list(enumerate(cosine_similarities[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    movie_indices = [i[0] for i in sim_scores]
+
+    return movie_title.iloc[movie_indices]
